375065b222b9c7420b103a1f89707ab666bc249f
Anders Olson <sredna43@gmail.com>
2021-07-19 22:12:00 -0500

    interpolation/extrapolation

--- utils/DedicatedServer.gd
+++ utils/DedicatedServer.gd
@@ -1,23 +1,41 @@
 extends Node
 
 var network = NetworkedMultiplayerENet.new()
-var ip = "127.0.0.1"
-var port = 1998
+var ip = "10.0.0.18"
+var port = 25565
 var connected = false
+var latency = 0.0
+var client_clock = 0
+var latency_array = []
+var delta_latency = 0
+var decimal_collector: float = 0
 
 func _ready():
 	connect_to_server()
+	
+func _physics_process(delta):
+	client_clock += int(delta * 1000) + delta_latency
+	delta_latency = 0
+	decimal_collector += (delta * 1000) - int(delta * 1000)
+	if decimal_collector >= 1.00:
+		client_clock += 1
+		decimal_collector -= 1.00
 
 func connect_to_server():
 	var _create_client_error = network.create_client(ip, port)
 	get_tree().set_network_peer(network)
-	
 	var _connection_succes_signal_status = network.connect("connection_succeeded", self, "_on_connection_succeeded")
 	var _connection_fail_signal_status = network.connect("connection_failed", self, "_on_connection_failed")
 	
 func _on_connection_succeeded():
 	print("Connected to server " + str(ip) + ":" + str(port))
+	rpc_id(1, "fetch_server_time", OS.get_system_time_msecs())
 	connected = true
+	var timer = Timer.new()
+	timer.wait_time = 0.5
+	timer.autostart = true
+	timer.connect("timeout", self, "determine_latency")
+	self.add_child(timer)
 	
 func _on_connection_failed():
 	print("Failed to connect to server " + str(ip) + ":" + str(port))
@@ -35,3 +53,25 @@ remote func despawn_player(pid):
 
 remote func receive_world_state(world_state):
 	get_node("../SceneHandler").update_world_state(world_state)
+
+remote func return_server_time(server_time, client_time):
+	latency = (OS.get_system_time_msecs() - client_time) / 2
+	client_clock = server_time + latency
+	
+func determine_latency():
+	rpc_id(1, "determine_latency", OS.get_system_time_msecs())
+
+remote func return_latency(client_time):
+	latency_array.append((OS.get_system_time_msecs() - client_time) / 2)
+	if latency_array.size() == 9:
+		var total_latency = 0
+		latency_array.sort()
+		var mid_point = latency_array[4]
+		for i in range(latency_array.size() - 1, -1, -1):
+			if latency_array[i] > (2 * mid_point) and latency_array[i] > 20:
+				latency_array.remove(i)
+			else:
+				total_latency += latency_array[i]
+		delta_latency = (total_latency / latency_array.size()) - latency
+		latency = total_latency / latency_array.size()
+		latency_array.clear()
--- utils/SceneHandler.gd
+++ utils/SceneHandler.gd
@@ -4,6 +4,8 @@ var level1 = preload("res://scenes/levels/TestLevel_1.tscn")
 var player_spawn = preload("res://scenes/entities/player/PlayerTemplate.tscn")
 var level_instance: Node2D
 var last_world_state = 0
+var world_state_buffer: Array = []
+var interpolation_offset = 100
 
 func _ready():
 	level_instance = level1.instance()
@@ -19,15 +21,44 @@ func spawn_player(pid: int, spawn_position: Vector2) -> void:
 		level_instance.get_node("YSort/OtherPlayers").add_child(new_player)
 
 func despawn_player(pid):
+	yield(get_tree().create_timer(0.2), "timeout")
 	level_instance.get_node("YSort/OtherPlayers/" + str(pid)).queue_free()
-
+	
 func update_world_state(world_state):
 	if world_state["T"] > last_world_state:
 		last_world_state = world_state["T"]
-		world_state.erase("T")
-		world_state.erase(get_tree().get_network_unique_id())
-		for player in world_state.keys():
-			if level_instance.get_node("YSort/OtherPlayers").has_node(str(player)):
-				level_instance.get_node("YSort/OtherPlayers/" + str(player)).move_player(world_state[player]["P"])
-			else:
-				spawn_player(player, world_state[player]["P"])
+		world_state_buffer.append(world_state)
+		
+func _physics_process(_delta):
+	var render_time = OS.get_system_time_msecs() - interpolation_offset
+	if world_state_buffer.size() > 1:
+		while world_state_buffer.size() > 2 and render_time > world_state_buffer[2].T:
+			world_state_buffer.remove(0)
+		if world_state_buffer.size() > 2:
+			var interpolation_factor = float(render_time - world_state_buffer[1]["T"]) / float(world_state_buffer[2]["T"] - world_state_buffer[1]["T"])
+			for player in world_state_buffer[2].keys():
+				if str(player) == "T":
+					continue
+				if player == get_tree().get_network_unique_id():
+					continue
+				if not world_state_buffer[1].has(player):
+					continue
+				if level_instance.get_node("YSort/OtherPlayers").has_node(str(player)):
+					var new_position = lerp(world_state_buffer[1][player]["P"], world_state_buffer[2][player]["P"], interpolation_factor)
+					level_instance.get_node("YSort/OtherPlayers/" + str(player)).move_player(new_position)
+				else:
+					print("Spawning player " + str(player))
+					spawn_player(player, world_state_buffer[2][player]["P"])
+		elif render_time > world_state_buffer[1].T:
+			var extrapolation_factor = float(render_time - world_state_buffer[0]["T"]) / float(world_state_buffer[1]["T"] - world_state_buffer[0]["T"]) - 1.00
+			for player in world_state_buffer[1].keys():
+				if str(player) == "T":
+					continue
+				if player == get_tree().get_network_unique_id():
+					continue
+				if not world_state_buffer[0].has(player):
+					continue
+				if level_instance.get_node("YSort/OtherPlayers").has_node(str(player)):
+					var position_delta = (world_state_buffer[1][player]["P"] - world_state_buffer[0][player]["P"])
+					var new_position = world_state_buffer[1][player]["P"] + (position_delta * extrapolation_factor)
+					level_instance.get_node("YSort/OtherPlayers/" + str(player)).move_player(new_position)
